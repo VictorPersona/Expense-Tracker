0a6e62c0bddfa6f815a63756a7a357c4ef10bfcf
Gaurav <vapourise00@gmail.com>
2025-02-05 18:39:41 +0530

    replaced expense date with controlled form

--- src/components/CreateExpense.jsx
+++ src/components/CreateExpense.jsx
@@ -1,48 +1,69 @@
 import React, { useEffect, useState } from 'react'
 
 function CreateExpense({ addExpense }) {
-  const [expenseType, setExpenseType] = useState('')
-  const [expenseDate, setExpenseDate] = useState('')
-  const [expenseAmount, setExpenseAmount] = useState('')
-  const typeOnChange = (e) => {
-    setExpenseType(e.target.value)
-  }
-  const amountOnChange = (e) => {
-    setExpenseAmount(e.target.value)
-  }
-  const dateOnChange = (e) => {
-    setExpenseDate(e.target.value)
+  const [expenseData, setExpenseData] = useState({
+    type: '',
+    amount: '',
+    date: '',
+  })
+  const handleOnChange = (e) => {
+    const { name, value } = e.target
+    setExpenseData((prevExpense) => ({
+      ...prevExpense,
+      [name]: value,
+    }))
   }
 
   const handleOnClick = () => {
+    if (
+      expenseData.amount == '' ||
+      expenseData.date == '' ||
+      expenseData.type == ''
+    ) {
+      alert('Please fill all the details')
+      return
+    }
     const expenseToBeAdded = {
       id: crypto.randomUUID(),
-      type: expenseType,
-      amount: expenseAmount,
-      date: expenseDate,
+      type: expenseData.type,
+      amount: expenseData.amount,
+      date: expenseData.date,
     }
     addExpense(expenseToBeAdded)
+
+    setExpenseData({
+      type: '',
+      amount: '',
+      date: '',
+    })
   }
   useEffect(() => {
     const today = new Date()
     const formattedDate = today.toISOString().split('T')[0]
-    setExpenseDate(formattedDate)
+    setExpenseData((prevExpense) => ({ ...prevExpense, date: formattedDate }))
   }, [])
   return (
     <div>
       <input
         type="text"
-        value={expenseType}
+        value={expenseData.type}
+        name="type"
         placeholder="Type"
-        onChange={typeOnChange}
+        onChange={handleOnChange}
       />
       <input
         type="number"
-        value={expenseAmount}
+        value={expenseData.amount}
         placeholder="Amount"
-        onChange={amountOnChange}
+        name="amount"
+        onChange={handleOnChange}
       />
-      <input type="date" value={expenseDate} onChange={amountOnChange} />
+      <input
+        type="date"
+        value={expenseData.date}
+        name="date"
+        onChange={handleOnChange}
+      />
       <button type="button" className="btn btn-primary" onClick={handleOnClick}>
         Create
       </button>
